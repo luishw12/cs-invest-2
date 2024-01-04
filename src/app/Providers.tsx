5431f31b00f612e2dd832d0316cee987463043a1
Luis Henrique Wendt <luishw08@gmail.com>
2024-01-04 17:42:19 -0300

    Ajustes

--- src/app/Providers.tsx
+++ src/app/Providers.tsx
@@ -4,10 +4,8 @@ import DefaultLayoutInterface from "@/types/defaultLayoutInterface";
 import {SessionProvider} from "next-auth/react";
 import {ToastContainer} from "react-toastify";
 import {UserContextProvider} from "@/context/UserContext";
-import {hashPassword} from "@/pages/api/user/create";
 
 export default function Providers({children}: DefaultLayoutInterface) {
-  console.log(hashPassword("123"))
   return (
     <SessionProvider>
       <UserContextProvider>
