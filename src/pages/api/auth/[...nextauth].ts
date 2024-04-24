47ed1241f664c82fc33e4400518b1310bdc45bde
Luis Henrique Wendt <luishw08@gmail.com>
2024-04-23 21:57:23 -0300

    arrumando db

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,68 +1,90 @@
-import NextAuth, {NextAuthOptions} from "next-auth";
+import Cors from "cors";
+import { NextApiHandler } from "next";
+import NextAuth from "next-auth";
 import CredentialsProvider from "next-auth/providers/credentials";
 
-export const authOptions: NextAuthOptions = {
-  providers: [
-    CredentialsProvider({
-      name: "Credentials",
-      credentials: {
-        email: {
-          label: "Email",
-          type: "email"
-        },
-        password: {
-          label: "Senha",
-          type: "password"
-        }
-      },
-      async authorize({email, password}: any) {
-        const userCredentials = {
-          email,
-          password,
-        };
+// Configure o middleware CORS
+const corsMiddleware = Cors({
+  origin: "*", // Permita solicitações de qualquer origem (mude conforme necessário)
+  methods: ["GET", "POST", "PUT", "DELETE"], // Permita os métodos HTTP necessários
+  allowedHeaders: ["Content-Type", "Authorization"], // Permita os cabeçalhos necessários
+});
 
-        const res = await fetch(
-          `${process.env.NEXTAUTH_URL}/api/user/auth`,
-          {
-            method: "POST",
-            body: JSON.stringify(userCredentials),
-            headers: {
-              "Content-Type": "application/json",
-            },
-          }
-        );
-
-        const user = await res.json();
-
-        if (res.ok && (!user.dateExpire || new Date() < new Date(user.dateExpire))) {
-          return user;
-        }
-        return null;
-      },
-    }),
-  ],
-
-  session: {
-    maxAge: 60 * 60 * 24 // 1 dia
-  },
-
-  pages: {
-    signIn: "/login",
-    signOut: "/login",
-  },
-
-  callbacks: {
-    async jwt({ token, user, session, trigger }) {
-      if (trigger === "update" && session.user.nome) {
-        token = {...token, ...session.user}
+// O manipulador de API NextAuth com middleware CORS
+const handler: NextApiHandler = async (req, res) => {
+  // Execute o middleware CORS
+  await new Promise((resolve, reject) => {
+    corsMiddleware(req, res, (result) => {
+      if (result instanceof Error) {
+        return reject(result);
       }
-      return { ...token, ...user };
+      return resolve(result);
+    });
+  });
+  // Passe a requisição para o NextAuth
+  return NextAuth(req, res, {
+    providers: [
+      CredentialsProvider({
+        name: "Credentials",
+        credentials: {
+          email: {
+            label: "Email",
+            type: "email"
+          },
+          password: {
+            label: "Senha",
+            type: "password"
+          }
+        },
+        async authorize({email, password}: any) {
+          const userCredentials = {
+            email,
+            password,
+          };
+
+          const res = await fetch(
+            `${process.env.NEXT_PUBLIC_NEXTAUTH_URL}/api/user/auth`,
+            {
+              method: "POST",
+              body: JSON.stringify(userCredentials),
+              headers: {
+                "Content-Type": "application/json",
+              },
+            }
+          );
+
+          const user = await res.json();
+
+          if (res.ok && (!user.dateExpire || new Date() < new Date(user.dateExpire))) {
+            return user;
+          }
+          return null;
+        },
+      }),
+    ],
+
+    session: {
+      maxAge: 60 * 60 * 24 // 1 dia
     },
-    async session({ session, token }) {
-      session.user = token;
-      return session;
-    }
-  },
+
+    pages: {
+      signIn: "/login",
+      signOut: "/login",
+    },
+
+    callbacks: {
+      async jwt({ token, user, session, trigger }) {
+        if (trigger === "update" && session.user.nome) {
+          token = {...token, ...session.user}
+        }
+        return { ...token, ...user };
+      },
+      async session({ session, token }) {
+        session.user = token;
+        return session;
+      }
+    },
+  });
 };
 
-export default NextAuth(authOptions);
+export default handler;
