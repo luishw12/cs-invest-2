f1f26cbd5d7969fea66e4b7ac1a5a1430ca1fbcb
Luis Henrique Wendt <luishw08@gmail.com>
2024-04-23 22:27:58 -0300

    arrumando db

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -43,7 +43,7 @@ const handler: NextApiHandler = async (req, res) => {
           };
 
           const res = await fetch(
-            `${process.env.NEXT_PUBLIC_NEXTAUTH_URL}/api/user/auth`,
+            `${process.env.NEXTAUTH_URL}/api/user/auth`,
             {
               method: "POST",
               body: JSON.stringify(userCredentials),
